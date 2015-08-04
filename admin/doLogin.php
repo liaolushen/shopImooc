b1e0125338299760f571d7807d33a9a23b4eed91
liaolushen <1219810894@qq.com>
2015-08-04 19:32:05 +0800

    auto login

--- admin/doLogin.php
+++ admin/doLogin.php
@@ -4,10 +4,15 @@ $username = $_POST['username'];
 $password = md5($_POST['password']);
 $verify = $_POST['verify'];
 $verify1 = $_SESSION['verify'];
+@$autoFlag = $_POST['autoFlag']; // 可能有警告，直接忽略
 if (strtolower($verify) == strtolower($verify1)) {
     $sql = "select * from imooc_admin where username='{$username}' and password = '{$password}'";
     $row = checkAdmin($sql);
     if ($row) {
+        if ($autoFlag) {
+            setcookie("adminId", $row['id'], time() + 7 * 24 * 3600);
+            setcookie("adminName", $row['username'], time() + 7 * 24 * 3600);
+        }
         $_SESSION['adminName'] = $row['username'];
         $_SESSION['adminId'] = $row['id'];
         header("location:index.php");
--- core/admin.inc.php
+++ core/admin.inc.php
@@ -4,7 +4,7 @@ function checkAdmin($sql) {
 }
 
 function checkLogined() {
-    if ($_SESSION['adminId'] == "") {
+    if (@$_SESSION['adminId'] == "" && $_COOKIE['adminId'] == "") {
         header("location:login.php");
     }
 }
@@ -13,6 +13,12 @@ function logout() {
     $_SESSION = array();
     if (isset($_COOKIE[session_name()])) {
         setcookie(session_name(), "", time() - 1);
+    }
+    if (isset($_COOKIE["adminId"])) {
+        setcookie("adminId", "", time() - 1);
+    }
+    if (isset($_COOKIE["adminName"])) {
+        setcookie("adminName", "", time() - 1);
     }
     session_destroy();
     header("location:login.php");
